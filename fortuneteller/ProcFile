bde5acae706ca26013f79ddd99f2fa7a3493cfb6
eddieward21 <eddie.musashi1@gmail.com>
2022-01-12 14:55:16 -0500

    deploying to heroku

---
+++ fortuneteller/ProcFile
@@ -0,0 +1,1 @@
+web: gunicorn fortuneteller.wsgi --log-file -
--- fortuneteller/settings.py
+++ fortuneteller/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zv#7p$bd^d_f$30n_$ur$v($gdgyfa5yv#$ke1lx-w6yj)q=^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eddie-fortuneteller.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
